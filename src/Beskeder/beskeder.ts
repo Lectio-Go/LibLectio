1106148faadc7c3de9894017d32f7828e85c348a
Niklas Haim <haim@Niklass-MacBook-Pro.local>
2021-01-27 20:29:55 +0100

    Nu kan den få alle de informationer der er uden for beksed

--- src/Beskeder/beskeder.ts
+++ src/Beskeder/beskeder.ts
@@ -41,7 +41,52 @@ export async function hentBeskedliste(user: AuthenticatedUser, requestHelper: Le
           besked.Aaben = true;
         }
       }
+      besked.Emne = beskedTable('td:nth-child(4)').text().replace(/\t/g, '').replace(/\n/g, '');
+      besked.Seneste_besked = beskedTable('td:nth-child(5)').text().replace(/\t/g, '').replace(/\n/g, '');
+      besked.Foerste_besked = beskedTable('td:nth-child(6)').text().replace(/\t/g, '').replace(/\n/g, '');
+
+      let modtagerString = beskedTable('td:nth-child(7)').html();
+      besked.Modtagere = [];
+      if(modtagerString !== null){
+          modtagerString = modtagerString.substring(modtagerString.lastIndexOf('<span title="') + 13, modtagerString.lastIndexOf('" class="tooltip"><img src="/lectio/img/class.auto'));
+          const index = (modtagerString.match(/\n/g)||[]).length+1;
+          for(let k = 0; k < index; k++ ){
+            let dString: string = modtagerString.substring(modtagerString.lastIndexOf('\n')+1);
+            besked.Modtagere.push(dString);
+            modtagerString = modtagerString.replace('\n'+dString, '')
+          }
+      }
+
+      const dateString = beskedTable('td:nth-child(8)').text();
+      if(dateString !== null){
+        if(dateString.length === 5){
+          besked.Aendret = parse(dateString, 'HH:mm', new Date());
+        }
+        else if(dateString.length === 8){
+          if(dateString.indexOf(':')=== -1){
+            besked.Aendret = parse(dateString.substring(dateString.lastIndexOf('/')-2), 'd/M', new Date());
+          }
+          else{
+            besked.Aendret = parse(dateString.substring(dateString.lastIndexOf(':')-2), 'HH:mm', new Date().setDate(new Date().getDate()-1));
+          }
+        }
+        else{
+          besked.Aendret = parse(dateString, 'd/M-yyyy', new Date());
+        }
+      }
+      
+      let idString = beskedTable('td:nth-child(4)').children().html();
+      if(idString !== null){
+        idString = idString?.substring(idString.lastIndexOf('$LB2$_MC_$_')+11, idString.lastIndexOf('); return false')-1);
+        besked.Id = idString.replace('&apos', '');
+      }
+      
+
+
+
       beskeder.push(besked);
+      
+
     }
   
     beskeder.shift();
